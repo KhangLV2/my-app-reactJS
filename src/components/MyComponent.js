2d620c31b1216148410e797aa8ee99a8e8e6a37b
KhangLV2 <lechankhang10012002@gmail.com>
2025-07-09 16:07:54 +0700

    su kien

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,11 +10,67 @@ class MyComponent extends React.Component {
     age: 27,
   };
 
+  //c1
+  //   handleClick = (event) => {
+  //     console.log(">> Click me my button");
+  //     console.log("My name is ", this.state.name);
+  //   };
+
+  handleClick(event) {
+    console.log(">> Click me my button");
+    // console.log(" random ", Math.floor(Math.random() * 100 + 1));
+
+    //merge State=> react class
+    this.setState({
+      name: "Eric",
+      age: Math.floor(Math.random() * 100 + 1),
+    });
+  }
+
+  handleOnMouseOver(event) {
+    console.log(event);
+  }
+
+  handleChangInput = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+    console.log(event.target.value);
+  };
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   // JSX
   render() {
     return (
       <div>
-        my name is {this.state.name} and I am from {this.state.address}
+        my name is {this.state.name} and I am {this.state.age}
+        {/* <button onMouseOver={this.handleOnMouseOver}>Click me</button> */}
+        {/* <button onClick={this.handleClick}>Click me</button> */}
+        {/* C2 */}
+        {/* <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button> */}
+        <form
+          onSubmit={(event) => {
+            this.handleOnSubmit(event);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(event) => {
+              this.handleChangInput(event);
+            }}
+          ></input>
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
